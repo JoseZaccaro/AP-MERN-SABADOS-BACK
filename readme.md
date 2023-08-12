f485096ebe1927ecf97f8fdb1f9b0392eacc9bd3
Jose Zaccaro <a.zaccaro42@gmail.com>
2023-08-12 14:05:49 -0300

    readme

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Repositorio de backend de Mindflix el proyecto paralelo de los sabados. 
+## en las diferentes ramas estan los proyectos segun el horario. 10 - 12 y 12 - 14. 
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-const router = Router()
-import seriesRouter from './seriesRouter.js';
-import peliculasRouter from './peliculasRouter.js';
-
-router.use('/series', seriesRouter)
-router.use('/peliculas', peliculasRouter)
-
-export default router
