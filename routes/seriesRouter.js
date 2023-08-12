edecb9e31fa018543f44bd14f73d522f0d0e402a
Jose Zaccaro <a.zaccaro42@gmail.com>
2023-08-12 12:13:26 -0300

    'initial

--- routes/seriesRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import seriesController from '../controllers/seriesController.js';
-const seriesRouter = Router()
-
-seriesRouter.get('/', seriesController.getAllSeries)
-
-
-export default seriesRouter
